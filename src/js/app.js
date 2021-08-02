f5b9b33c428349d67e4e7adcbf42fcc82a50a3be
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-08-02 16:44:37 -0500

    reset commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,66 @@
-// import * as Tone from 'tone';
+const song = document.querySelector(".song");
+const play = document.querySelector(".play");
+const video = document.querySelector(".vid-container video");
+const sounds = document.querySelectorAll(".sound-picker button");
+const timeDisplay = document.querySelector(".time-display");
+const timeSelect = document.querySelectorAll(".time-select button");
+let fakeDuration = 600;
 
-window.addEventListener("load", () => {
-  const beats = document.querySelectorAll(".beat");
-  const dots = document.querySelectorAll(".dots div");
-  const visual = document.querySelector(".visual");
-  const colors = [
-    "#920000",
-    "#b90000",
-    "#ff0000",
-    "#f53131",
-    "#ffc400",
-    "#fffb00",
-    "#ffee00",
-    "#88ff00",
-    "#1eff00",
-    "#00ff6a",
-    "#00ffd5",
-    "#00f5fd",
-    "#00ccff",
-    "#0084ff",
-    "#0059ff",
-    "#1900ff",
-    "#4c00ff",
-    "#9900ff",
-    "#8500b9",
-    "#e100ff",
-    "#ff00c8",
-    "#ff004c",
-  ];
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+  fakeDuration % 60
+)}`;
 
-
-
-  dots.forEach((dot, index) => {
-    dot.addEventListener("click", function() {
-      beats[index].currentTime = 0;
-      beats[index].play();
-      createBubble(index);
-
-
-    });
-
+sounds.forEach(sound => {
+  sound.addEventListener("click", function() {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
   });
-
-// Create bubbles
-  const createBubble = index => {
-    const bubble = document.createElement("div");
-    visual.appendChild(bubble);
-    bubble.style.backgroundColor = colors[index];
-    bubble.style.animation = `jump 1s ease`;
-    bubble.addEventListener("animationend", function() {
-      visual.removeChild(this);
-    });
-  };
-
-
 });
 
+play.addEventListener("click", function() {
+  checkPlaying(song);
+});
 
+const restartSong = song =>{
+    let currentTime = song.currentTime;
+    song.currentTime = 0;
+    console.log("ciao")
+
+}
+
+timeSelect.forEach(option => {
+  option.addEventListener("click", function() {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      fakeDuration % 60
+    )}`;
+  });
+});
+
+const checkPlaying = song => {
+  if (song.paused) {
+    song.play();
+    video.play();
+    play.src = "./svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
+song.ontimeupdate = function() {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    song.currentTime = 0;
+    play.src = "./svg/play.svg";
+    video.pause();
+  }
+};
